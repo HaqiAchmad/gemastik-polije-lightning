fa4e0c46f528706f05b0d730d7dc93306347a1f4
Achmad Baihaqi <baihaqi.myapps@gmail.com>
2022-10-14 23:53:06 +0700

    menambahkan id2 di setiap tabel user

--- src/com/users/UserData.java
+++ src/com/users/UserData.java
@@ -1,7 +1,7 @@
 package com.users;
 
 /**
- * Field-field yang ada didalam tabel users pada <b>Database</b>.
+ * Field-field yang ada didalam tabel user pada <b>Database</b>.
  *
  * @author Achmad Baihaqi
  * @since 2021-06-14
@@ -9,12 +9,16 @@ package com.users;
 public enum UserData {
     
     ID_USER,
+    ID_ADMIN,
+    ID_KARYAWAN,
+    ID_SUPPLIER,
+    ID_PEMBELI,
     PASSWORD,
     LEVEL,
     NAMA_ADMIN,
     NAMA_PETUGAS,
     NAMA_SUPPLIER,
     NAMA_PEMBELI,
-    NO_HP,
+    NO_TELP,
     ALAMAT
 }
